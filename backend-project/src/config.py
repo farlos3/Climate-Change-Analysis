e823a13c2a454c663c1d7542bb67ed63ed226931
farlos <thiraphat.pant@gmail.com>
2025-11-22 02:24:59 +0700

    Create FastAPI&Airflow webserver(for dev)

---
+++ backend-project/src/config.py
@@ -0,0 +1,23 @@
+LAT = 13.736717
+LON = 100.523186
+
+POWER_URL = "https://power.larc.nasa.gov/api/temporal/daily/point"
+
+DEFAULT_START = "19810101"
+AFTER_2001_START = "20010101"
+
+# parameters ที่เริ่มมีข้อมูลหลังปี 2001
+PARAMETERS_AFTER_2001 = [
+    "CLRSKY_SFC_PAR_DIRH",
+    "CLRSKY_SFC_PAR_DIFF",
+    "CLRSKY_SFC_LW_UP",
+    "CLRSKY_SFC_LW_DWN",
+    "CLRSKY_NKT",
+    "CLRSKY_KT",
+    "CLRSKY_DAYS",
+    "CLOUD_OD",
+    "CLOUD_AMT_NIGHT",
+    "CLOUD_AMT_DAY",
+    "CLOUD_AMT",
+    "CDD18_3"
+]
